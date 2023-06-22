469512ef2c4eac6bb4126e09750ee3594d1fc264
mexisme <wildjim+dev@kiwinet.org>
2023-06-22 17:15:57 +1200

    Extract to classes

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,71 @@ import * as core from '@actions/core';
 import * as github from '@actions/github';
 import {RequestError} from '@octokit/request-error';
 
+type RefType = 'tag' | 'branch';
+
+class FindRef {
+  readonly githubToken: string;
+  readonly octokit;
+  readonly owner: string;
+  readonly repo: string;
+
+  constructor(token: string) {
+    this.githubToken = token;
+    this.octokit = github.getOctokit(this.githubToken);
+    ({owner: this.owner, repo: this.repo} = github.context.repo);
+  }
+
+  #refPrefix(refType: RefType): String {
+    const refPrefix: {[id: string]: string} = {
+      tag: 'tags/',
+      branch: 'heads/',
+    };
+
+    return refPrefix[refType as string];
+  }
+
+  async find(refType: RefType, ref: String): Promise<boolean> {
+    const refPrefix = this.#refPrefix(refType);
+
+    try {
+      core.debug(`Searching for ${refType} '${ref}'`);
+
+      await this.octokit.rest.git.getRef({
+        owner: this.owner,
+        repo: this.repo,
+        ref: `${refPrefix}/${ref}`,
+      });
+    } catch (error) {
+      if (error instanceof Error) core.debug(`error = ${error.message}`);
+      core.debug(
+        `error.type = ${typeof error} (RequestError = ${typeof RequestError})`
+      );
+
+      if (error instanceof RequestError) {
+        core.debug(`error.status = ${error.status}`);
+
+        if (error.status === 200) {
+          core.debug(`Matching ${refType} was found`);
+          return true;
+        } else if (error.status === 404) {
+          core.debug('Matching ${refType} was not found');
+        } else {
+          core.debug(`Unexpected request error = '${error}'`)
+          throw error;
+        }
+      } else {
+        core.debug(`Unexpected error = '${error}'`);
+        throw error;
+      }
+    }
+
+    return false;
+  }
+}
+
 async function run(): Promise<void> {
   try {
-    const {owner, repo} = github.context.repo;
-    const octokit = github.getOctokit(core.getInput('github_token'));
+    const findRef = new FindRef(core.getInput('github_token'));
 
     const gitTag: string = core.getInput('tag');
     const gitBranch: string = core.getInput('branch');
@@ -13,46 +74,14 @@ async function run(): Promise<void> {
 
     const found: String[] = [];
 
-    if (gitTag !== null && gitTag !== '') {
-      try {
-        core.debug(`Searching for tag ${gitTag}`);
-
-        await octokit.rest.git.getRef({
-          owner,
-          repo,
-          ref: `tags/${gitTag}`,
-        });
-      } catch (error) {
-        if (error instanceof RequestError) {
-          if (error.status === 200) {
-            core.debug('Matching Tag was found');
-            found.push('BRANCH');
-          } else if (error.status === 404) {
-            core.debug('Matching Tag was not found');
-          } else throw error;
-        } else throw error;
-      }
-    }
-
-    if (gitBranch !== null && gitBranch !== '') {
-      try {
-        core.debug(`Searching for branch ${gitBranch}`);
-        await octokit.rest.git.getRef({
-          owner,
-          repo,
-          ref: `heads/${gitBranch}`,
-        });
-      } catch (error) {
-        if (error instanceof RequestError) {
-          if (error.status === 200) {
-            core.debug('Matching Branch was found');
-            found.push('BRANCH');
-          } else if (error.status === 404) {
-            core.debug('Matching Branch was not found');
-          } else throw error;
-        } else throw error;
-      }
-    }
+    if (gitTag !== null && gitTag !== '' && await findRef.find('tag', gitTag))
+      found.push('TAG');
+    if (
+      gitBranch !== null &&
+      gitBranch !== '' &&
+      await findRef.find('branch', gitBranch)
+    )
+      found.push('BRANCH');
 
     const foundList = found.join(', ');
     core.setOutput('found', foundList);
